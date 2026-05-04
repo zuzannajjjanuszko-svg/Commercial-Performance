d551de5f26c7afc117e8e1eafbeac6a29d461619
Kevin Manach <kevinmmanach@gmail.com>
2026-05-04 13:55:08 +0200

    Remove temporary patch scripts

--- remove_dupes.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('fact_revenue_actuals.csv')
-df.drop_duplicates(inplace=True)
-df.to_csv('fact_revenue_actuals.csv', index=False)
-print("Duplicates removed. Rows:", len(df))
--- resolve_ip.py
+++
@@ -1,7 +0,0 @@
-import socket
-# try the pooler host that worked in the populate script
-try:
-    ip = socket.gethostbyname("aws-0-eu-west-1.pooler.supabase.com")
-    print("IP:", ip)
-except Exception as e:
-    print("Error:", e)
